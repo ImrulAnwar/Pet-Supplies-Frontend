b44b13c79dfaa0fe18b433b4b80fed3df89a816a
Imrul Anwar <imrulanwar836@gmail.com>
2023-08-05 08:28:30 +0600

    navbar is better now

--- src/layouts/Navbar/Navbar.jsx
+++ src/layouts/Navbar/Navbar.jsx
@@ -9,28 +9,31 @@ import profileIcon from '../../assets/icons/profile.png';
 import Searchbar from '../Searchbar/Searchbar.jsx'; 
 
 const Navbar = () => {
-
-
   return (
     <nav className='navbar'>
-       <ul>
-        <li>
-            <ul>
-                <li className="logo"><img src={logo} alt="logo" /></li>
-            </ul>
-        </li>
-        <ul className="middle-sections">
-            <li><Link to="/"><img className = "home icon" src={homeIcon} alt="home" />Home</Link></li>
-            <li><Link to="/shop"><img className = "shop icon" src={shopIcon} alt="profile" />Shop</Link></li>
-        </ul>
-        <ul className='icons'>
-          <li><Searchbar/></li>
-          <li><Link className='cart-icon' to="/register"><img className = "cart icon"  src={cartIcon} alt="cart" /></Link></li>
-          <li><Link className='profile-icon' to="/register"><img className = "profile icon"  src={profileIcon} alt="profile" /></Link></li>
-        </ul>
-       </ul> 
+      <div className="container">
+        <div className="left-section">
+          <ul>
+            <li className="logo"><img className = "logo icon" src={logo} alt="logo" /></li>
+          </ul>
+        </div>
+        <div className="middle-section">
+          <ul>
+            <li><Link to="/"><img className="home icon" src={homeIcon} alt="home" />Home</Link></li>
+            <li><Link to="/shop"><img className="shop icon" src={shopIcon} alt="profile" />Shop</Link></li>
+          </ul>
+        </div>
+        <div className="right-section">
+          <ul className='icons'>
+            <li><Searchbar /></li>
+            <li><Link className='cart-icon' to="/register"><img className="cart icon" src={cartIcon} alt="cart" /></Link></li>
+            <li><Link className='profile-icon' to="/register"><img className="profile icon" src={profileIcon} alt="profile" /></Link></li>
+          </ul>
+        </div>
+      </div>
     </nav>
-  );
-};
+  )
+}
+
 
 export default Navbar;
