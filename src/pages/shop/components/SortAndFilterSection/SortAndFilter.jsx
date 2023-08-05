919d35ecaf81740f035fc917e84234f1bbecd5d2
Imrul Anwar <imrulanwar836@gmail.com>
2023-08-05 18:12:40 +0600

    working on the shop page

--- src/pages/shop/components/SortAndFilterSection/SortAndFilter.jsx
+++ src/pages/shop/components/SortAndFilterSection/SortAndFilter.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
 import './SortAndFilter.css'
 
-function SortAndFilter() {
+const SortAndFilter = () => {
+  const options = ['Option 1', 'Option 2', 'Option 3']
+
   return (
     <div className='left-shop'>
-      <div className="left-box"></div>
+      <div className="left-box">
+        <div className="sort-by-box">
+            <h1>Sort</h1>
+            <select id="dropdown">
+                {options.map((option, index) => (
+                  <option key={index} value={`option${index + 1}`}>
+                    {option}
+                  </option>
+                ))}
+            </select>   
+        </div>
+      </div>
     </div>    
   );
 }
