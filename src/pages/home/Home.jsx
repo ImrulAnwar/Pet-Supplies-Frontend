5faf6c69437d0b641c6a05f25daab5f651eb6c06
Imrul Anwar <imrulanwar836@gmail.com>
2023-08-07 12:12:02 +0600

    featured products are working now

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -4,14 +4,41 @@ import CategorySection from './components/CategorySection/CategorySection'
 import FeaturedSection from './components/FeaturedSection/FeaturedSection'
 import WhatWeOffer from './components/WhatWeOffer/WhatWeOffer'  
 import OurProduct from './components/OurProducts/OurProduct'
+import { fetchAllItems } from '../../services/item_api'
+import React, {useState, useEffect} from 'react'
+
+function shuffleArray(array) {
+  // Fisher-Yates shuffle algorithm
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+  return array;
+}
 
 function Home() {
+  const [items, setItems] = useState([])
+
+  useEffect(() => {
+    fetchAllItems()
+      .then((data) => {
+        setItems(data)
+        setFilteredItems(data)
+        console.log('Response data:', data)
+      })
+  }, [])
+
+  const shuffledItems = shuffleArray(items);
+  const featuredItems = shuffledItems.slice(0, 5);
+
   return (
     <div>
       <Line/>
       <WelcomeSection/>
       <Line/>
       <CategorySection/>
+      <Line/>
+      <FeaturedSection items = {featuredItems}/>
       <Line/>
       <WhatWeOffer/>
       <Line/>
--- src/pages/home/components/FeaturedSection/FeaturedSection.jsx
+++ src/pages/home/components/FeaturedSection/FeaturedSection.jsx
@@ -1,9 +1,18 @@
 import './FeaturedSection.css'
+import ProductItem from '../../../../layouts/ProductItem/ProductItem'
 
-function FeaturedSection() {
+function FeaturedSection({items}) {
     return (
-        <div>
-            FeaturedSection
+        <div className='featured-box'>
+            <h1>Featured Products</h1>
+            <div className="featured-items">    
+                {items.map((item) => (
+                    <ProductItem key={item.slug} item={item} />
+                ))}
+            </div>
+            <p className="view-all">
+                View all &gt;
+            </p>
         </div>
     )
 }
