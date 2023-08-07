7b01ecd9d8f19152cfb573f591c272fd0207365f
Imrul Anwar <imrulanwar836@gmail.com>
2023-08-07 11:10:58 +0600

    added the footer without animation

--- src/layouts/Footer/Footer.jsx
+++ src/layouts/Footer/Footer.jsx
@@ -1,9 +1,34 @@
 import './Footer.css'
+import instagram from '../../assets/icons/instagram.png'
+import facebook from '../../assets/icons/facebook.png'
+import twitter from '../../assets/icons/twitter.png'
+import pinterest from '../../assets/icons/pinterest.png'
 
 function Footer() {
     return (
-        <div>
-            Footer
+        <div className='footer'>
+            <div className="left-footer">
+                <h2>
+                    About US
+                </h2>
+                <p> Terms & Conditions<br/>
+                    Privacy Policy<br/>
+                    Shipping Policy<br/>
+                    Special Purchase Option Policy.<br/><br/>
+
+                    © 2023 Pet Inn<br/>
+                    Powered by NoOne (It is a Demo Application)
+                </p>
+            </div>
+            <div className="right-footer">
+                <h2>Follow Us</h2>
+                <div className="socials">
+                    <img src={instagram} alt="" />
+                    <img src={facebook} alt="" />
+                    <img src={pinterest} alt="" />
+                    <img src={twitter} alt="" />
+                </div>
+            </div>
         </div>
     )
 }
