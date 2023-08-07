23e0380ee0e07e7092b826c27a6ec7dc9623c6ba
Imrul Anwar <imrulanwar836@gmail.com>
2023-08-07 10:40:17 +0600

    now will add the footer

--- src/pages/cart/Cart.jsx
+++ src/pages/cart/Cart.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Line from '../../layouts/Line/Line'
+import './Cart.css'
 
 function Cart() {
   return (
-    <>
+    <div className='cart-page'>
       <Line/>
-      <h1>This is SHOP</h1>
-    </>
+      <h1>This is Cart</h1>
+    </div>
   );
 }
 
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Line from '../../layouts/Line/Line'
+import './Profile.css'
 
 function Profile() {
   return (
-    <>
+    <div className='profile-page'>
       <Line/>
       <h1>This is Profile</h1>
-    </>
+    </div>
     
     
   );
